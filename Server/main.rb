100f5162e6da114430e3aa9e8bc0f2942d5a9400
Jonathan Leach <jonathan@Jonathans-MacBook-Pro.local>
2014-11-12 22:35:51 +0000

    more server changes

--- Server/main.rb
+++ Server/main.rb
@@ -7,17 +7,19 @@ class Node
 	
 	@@nodes = 0
 
-	def initialize(owner, name, lat, long)
+	def initialize(owner, name, lat, long, level, faction)
 		@owner = owner
 		@name = name
 		@lat = lat
 		@long = long
+		@level = level
+		@faction = faction
 		@@nodes = @@nodes + 1
 		@id = @@nodes
 	end
 
 	def out
-		{:id => @id, :owner => @owner, :name => @name, :lat => @lat, :long => @long}
+		{:id => @id, :owner => @owner, :name => @name, :lat => @lat, :long => @long, :level => @level, :faction => @faction}
 	end
 
 end
@@ -25,6 +27,8 @@ end
 class User
 	
 	attr_reader :openid
+	attr_reader :level
+
 	
 	@@users = 0
 
@@ -40,21 +44,26 @@ class User
 	def out
 		{:id => @id, :openid => @openid, :name => @name, :xp => @xp, :level => @level}
 	end
+	
+	def increaseXp(amount)
+		@xp = @xp + amount
+		@level = (@xp / 100) + 1
+	end
 
 end
 
 owner = "Jon"
 
-nodes= [Node.new(owner,'Thames Barrier Park', 51.500189, 0.039983), 
-		Node.new(owner,'Connaught Bridge',51.506787,0.039983), 
-		Node.new(owner, 'Silvertown', 51.502834, 0.038996), 
-		Node.new(owner, 'The Crystal', 51.507241,0.016294), 
-		Node.new(owner,'Royal Victoria Gardens',51.499708, 0.067062)]
+nodes= [Node.new(owner,'Thames Barrier Park', 51.500189, 0.039983,1,1), 
+		Node.new(owner,'Connaught Bridge',51.506787,0.039983,2,2), 
+		Node.new(owner, 'Silvertown', 51.502834, 0.038996,2,2), 
+		Node.new(owner, 'The Crystal', 51.507241,0.016294,4,1), 
+		Node.new(owner,'Royal Victoria Gardens',51.499708, 0.067062,3,2)]
 		
-users= [User.new('fb990d3a9c8100889a2fb5b04567ec1f0ba086ce5e58da4abb513c12b30ed6ea','Jon L',99999999,9999999),
-		User.new('id1243','Will H',1,1),
-		User.new('id1276','Curtis K',1,1),
-		User.new('id1293','Peter E',1,1)]
+users= [User.new('fb990d3a9c8100889a2fb5b04567ec1f0ba086ce5e58da4abb513c12b30ed6ea','Jon L',195,2),
+		User.new('92490e15daa840a531b74050850e9bbe9639ddb879c52499fb807eedd84ae07e','Will H',105,2),
+		User.new('id1276','Curtis K',90,1),
+		User.new('id1293','Peter E',178,2)]
 
 get '/nodes' do
   json nodes.map { |n| n.out}
@@ -64,14 +73,56 @@ post '/nodes' do
 	request.body.rewind
 	request_payload = JSON.parse request.body.read
 	
-	newNode = Node.new(request_payload["owner"], request_payload["name"], request_payload["lat"], request_payload["long"])
+	user = users.find { |n| n.openid == request_payload["owner"] }
+	nodeLevel = user.level;
+	user.increaseXp(20)
+
+	
+	localIds = request_payload["locals"]
+	
+	localIds.each do |id|
+	
+		user = users.find { |n| n.openid == id }
+		if user == nil
+			nodeLevel = nodeLevel + 1
+		else
+			nodeLevel = nodeLevel + user.level
+			user.increaseXp(5)
+		end
+		
+	end
+		
+	newNode = Node.new(request_payload["owner"], request_payload["name"], request_payload["lat"], request_payload["long"], nodeLevel,1)
 	nodes = nodes.push(newNode)
-	"OK"
+
+	
+	json newNode.out
 
 end
 
 get '/users' do
 	json users.map { |n| n.out }
+end
+
+post '/userlist' do
+
+	request.body.rewind
+	request_payload = JSON.parse request.body.read
+	
+	requsers = request_payload["users"]
+	
+	foundUsers = requsers.map do |n|
+	
+		user = users.find { |m| m.openid == n }
+		
+		if user == nil
+			user = User.new(n,'Unknown',0,1)
+		end
+		
+		user
+	end
+	
+	json foundUsers.map { |n| n.out }
 end
 
 get '/users/:openid' do
